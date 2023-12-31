a6ed9418b6a219ce738aff795857b8cb2fdb672c
Anshika Soni <anshikasoni357@gmail.com>
2023-12-31 18:34:25 +0530

    Adding file

--- largestnumber.java
+++ largestnumber.java
@@ -1,5 +1,16 @@
 public class largestnumber{
     public static void main(String[] args){
+        int[] ar= {1, 2, 30, 4, 5};
+        int n = ar.length;
         
+        int maxi = ar[0]; // = 1
+        for(int i = 1; i<n;i++){
+            if(maxi<ar[i]){         
+                maxi = ar[i];
+            }
+        }
+        System.out.println(maxi);
+
+
     }
 }
