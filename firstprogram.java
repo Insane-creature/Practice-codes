13a48f5e9ae3dfe349f17cb071be23ea719a3402
Anshika Soni <anshikasoni357@gmail.com>
2024-01-01 17:34:04 +0530

    Adding twosum files

--- firstprogram.java
+++ firstprogram.java
@@ -1,4 +1,3 @@
-package Practicecodes;
 public class firstprogram {
     public static void main(String[] args){
         System.out.println("This is first java program");
---
+++ twosums.java
@@ -0,0 +1,20 @@
+public class twosums {
+    public static void main(String[] args){
+        int[] ar = {2,7,11,15};
+        int n = ar.length;
+        int target = 9;
+        int total = 0;
+        int[] result = new int[2];
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(ar[i] + ar[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        System.out.println(result[0] + " "+result[1]);
+    }
+    
+}
