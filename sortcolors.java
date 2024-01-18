f977e50c14545c3d3fbd521c5b9e7b7b35497d93
Anshika Soni <anshikasoni357@gmail.com>
2024-01-18 09:29:20 +0530

    Wave array

--- sortcolors.java
+++ sortcolors.java
@@ -5,21 +5,17 @@ public class sortcolors {
 
         for(int i = 0; i < n-1; i++){
             for(int j = 0; j < n - 1; j++){
-                int mini = nums[i];
+                int mini = nums[i];         // mini = 2
                 if(mini > nums[j+1]){
-                    int temp = mini;
-                    mini = nums[j+1];
-                    nums[j+1] = temp;
+                    int temp = mini;        // temp = 2
+                    mini = nums[j+1];       // mini = 1
+                    nums[j+1] = temp;       // nums[j+1] = 2
                 }
             }
         }
 
-
         for(int i = 0; i < n; i++){
             System.out.println(nums[i]);
         }
-
-
-
     }
 }
---
+++ wavearray.java
@@ -0,0 +1,17 @@
+public class wavearray {
+    public static void main(String[] args){
+        int[] a = {1,2,3,4,5};
+        int n = a.length;
+        for(int i = 0; i < n-1;i= i+2){
+            if(a[i+1]>a[i]){    // 2 > 1
+                int temp = a[i+1];
+                a[i+1] = a[i];
+                a[i] = temp;
+            }// 2 1 3 4 5
+        }
+        for(int i = 0; i < n;i++){
+            System.out.println(a[i]);
+
+        }
+    }
+}
