220c619edfb08e348192d60f889751e161aedfd3
Anshika Soni <anshikasoni357@gmail.com>
2024-01-30 19:23:08 +0530

    Updating file

--- InterviewPrep1/Array/removeduplicate.java
+++ InterviewPrep1/Array/removeduplicate.java
@@ -3,20 +3,14 @@ package InterviewPrep1.Array;
 public class removeduplicate {
     public static void main(String[] args) {
         int[] nums = {1,1,2};
-        int[] nums1 = new int[0];
-        int k = 0;
-        int total = 0;
-        for(int i = 0; i < nums.length - 1; i++){ // 1
-            for(int j = i+1; j < nums.length - i; j++){ // 2
-                if(nums[i] == nums[j]){ // 1 == 2
-                    k = k+1;
-                }
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];      // nums[1] = 1
+                j++;                    // j = 2
             }
         }
-        total = nums.length - k;
-        System.out.println(total);
-        for(int i = 0; i < nums1.length; i++){ // 1
-            System.out.println(nums1[i]);
-        }
+        System.out.println(j);
+        
     }
 }
